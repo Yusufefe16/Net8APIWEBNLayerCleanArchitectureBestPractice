053d1cb1634aadb91404e5ccde351a37debb5417
Yusuf <yefe1538@gmail.com>
2025-04-17 00:54:33 +0300

    Result Pattern

---
+++ Services/ServiceResult.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text.Json.Serialization;
+
+namespace App.Services;
+
+public class ServiceResult<T>
+{
+    public T? Data { get; set; }
+    public List<string>? ErrorMessage { get; set; }
+
+    public bool IsSuccess => ErrorMessage == null || ErrorMessage.Count == 0;
+    public bool IsFail => !IsSuccess;
+    
+    [JsonIgnore] public HttpStatusCode Status { get; set; }
+
+    [JsonIgnore] public string? UrlAsCreated { get; set; }
+    
+    //static factory method
+    public static ServiceResult<T> Success(T data, HttpStatusCode status = HttpStatusCode.OK)
+    {
+        return new ServiceResult<T>()
+        {
+            Data = data,
+            Status = status
+        };
+    }
+
+    public static ServiceResult<T> SuccessAsCreated(T data, string urlAsCreated)
+    {
+        return new ServiceResult<T>()
+        {
+            Data = data,
+            Status = HttpStatusCode.Created,
+            UrlAsCreated = urlAsCreated
+        };
+    }
+
+
+    public static ServiceResult<T> Fail(string errorMessage,
+        HttpStatusCode status = HttpStatusCode.BadRequest)
+    {
+        return new ServiceResult<T>()
+        {
+            ErrorMessage = [errorMessage],
+            Status = status
+        };
+    }
+}
