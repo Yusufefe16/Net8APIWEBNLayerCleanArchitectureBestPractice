68d34a15be240c56e41d8782b0684d0b50ffb162
Yusuf <yefe1538@gmail.com>
2025-04-20 23:22:35 +0300

    Model Validation 1

--- Services/Extensions/ServiceExtensions.cs
+++ Services/Extensions/ServiceExtensions.cs
@@ -1,4 +1,7 @@
-﻿using App.Services.Products;
+﻿using System.Reflection;
+using App.Services.Products;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +12,10 @@ public static class ServiceExtensions
     public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<IProductService, ProductService>();
+
+        services.AddFluentValidationAutoValidation();
+        
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         return services;
     }
 }
